ead5dd9cf146ce1aac6c67bde6e03285767a61ae
Andy F18 <adrian.f18e@gmail.com>
2022-08-29 21:34:52 +0200

    First draft : menu animation

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ class SMLM:
         self.__root.iconbitmap(r"config/gia.ico")
         self.__root.resizable(False, False)
 
-        self.__size = {'h': int(self.__root.winfo_screenheight()/2-50), 'w': int(self.__root.winfo_screenwidth()/2)}
-        self.__root.state('zoomed')
+        self.__size = {'h': self.__root.winfo_screenheight(), 'w': self.__root.winfo_screenwidth()}
+        # self.__root.state('zoomed')
+        self.__root.attributes("-fullscreen", True)
         self.__root.geometry(f"{self.__size['w']}x{self.__size['h']}")
         self.__root.config(background=self.__color['bg1'])
 
@@ -106,7 +107,7 @@ class SMLM:
         self.__lHideShowMenu.bind('<Leave>', lambda event, lab=self.__lHideShowMenu: self.__hoverMenuL(lab))
         self.__lHideShowMenu.grid(column=0, row=7, sticky=tk.E, padx=10)
 
-        self.__fMenu.place(width=250, height=self.__root.winfo_screenheight() - 50)
+        self.__fMenu.place(width=250, height=self.__size['h'])
 
     def __hideShowMenu(self):
         v = 0.15
@@ -120,7 +121,7 @@ class SMLM:
                 x += v
                 # print(n)
                 time.sleep(0.01)
-                self.__fMenu.place(anchor=tk.NW, width=250, height=self.__root.winfo_screenheight() - 50, x=n)
+                self.__fMenu.place(anchor=tk.NW, width=250, height=self.__size['h'], x=n)
                 self.__fMenu.update()
 
         else:
@@ -132,10 +133,10 @@ class SMLM:
                 x += v
                 # print(n)
                 time.sleep(0.01)
-                self.__fMenu.place(anchor=tk.NW, width=250, height=self.__root.winfo_screenheight() - 50, x=n)
+                self.__fMenu.place(anchor=tk.NW, width=250, height=self.__size['h'], x=n)
                 self.__fMenu.update()
             # self.fMenu.grid_forget()
-            self.__lShow.place(anchor=tk.SW, x=0, y=self.__size['h'] * 2)
+            self.__lShow.place(anchor=tk.SW, x=10, y=self.__size['h']-20)
 
     def __hoverMenuE(self, widget):
         widget.config(font=self.__fonts['cal20'])
